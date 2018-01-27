86fc0132caa71dbd555e189d7438a65f259142d6
Andrew Hanichkovsky <WindOfRussia@users.noreply.github.com>
2018-01-27 14:28:30 -0500

    [#31 #33] checking if vue components are even useful
    added 2 components based off of code that looked like it could be reusable in home.blade.php template.
    
    Vue is a laravel default but we can just not use it, I'm messing about merely for the sake of it.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        app('session')->put('status', 'new message');
         return view('home');
     }
 }
---
+++ resources/assets/js/components/StatusToast.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="alert alert-success">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "notification-toast"
+    }
+</script>
+
+<style scoped>
+
+</style>
