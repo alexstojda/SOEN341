1468bbef6bd1e199770de349465c135ed1c100fa
niconoloco <nicolas.b.champagne@gmail.com>
2018-02-08 16:32:36 -0500

    added comments to tables

--- database/migrations/2018_02_08_205548_create_answers_table.php
+++ database/migrations/2018_02_08_205548_create_answers_table.php
@@ -15,11 +15,19 @@ class CreateAnswersTable extends Migration
     {
         Schema::create('answers', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('q_id');
-            $table->integer('userid');
-            $table->text('answer_body');
+            //Which question this answer belongs to
+            $table->integer('question_id')->unsigned()->nullable();
+            //User that posted this answer
+            $table->integer('user_id')->unsigned();
+            //Answer content
+            $table->multiLineString('answer_body');
+            //Is this the best answer
             $table->boolean('isBestAnswer');
+            // Generate timestamps for update and creation
             $table->timestamps('created_at');
+
+
+            $table->foreign('user_id','question_id')->references('id')->on('users')->onDelete('set null')->onUpdate('no action');
         });
     }
 
--- database/migrations/2018_02_08_210354_create_comments_table.php
+++ database/migrations/2018_02_08_210354_create_comments_table.php
@@ -15,11 +15,19 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('q_id');
-            $table->integer('a_id');
-            $table->integer('userid');
-            $table->text('comment_body');
+            //Question that this comment is related to or null
+            $table->integer('question_id')->unsigned()->nullable();
+            //Answer that this comment is related to or null
+            $table->integer('answer_id')->unsigned()->nullable();
+            //User that posted this comment
+            $table->integer('user_id')->unsigned();
+            //Content of comment
+            $table->multiLineString('comment_body');
+            // Generate timestamps for update and creation
             $table->timestamps('created_at');
+
+
+            $table->foreign('user_id','question_id','answer_id')->references('id')->on('users')->onDelete('set null')->onUpdate('no action');
         });
     }
 
