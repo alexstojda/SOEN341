f4a3c6f3d5b7b47076744bbf1110b61f7e8413f7
niconoloco <nicolas.b.champagne@gmail.com>
2018-02-08 18:09:15 -0500

    The database tables that I have been trying to add minus some things

--- database/migrations/2018_02_08_150404_create_question_table.php
+++ database/migrations/2018_02_08_150404_create_question_table.php
@@ -15,11 +15,19 @@ class CreateQuestionsTable extends Migration
     {
         Schema::create('question', function (Blueprint $table) {
             $table->increments('id');
-            $table->increments('q_id');
-            $table->integer('userid');
-            $table->text('q_head');
-            $table->text('q_body');
+            //Question identifier
+            $table->increments('question_id');
+            //User that posted this question
+            $table->integer('user_id')->unsigned();
+            //Question's main title
+            $table->text('question_head');
+            //Question content
+            $table->multiLineString('question_body');
+            // Generate timestamps for update and creation
             $table->timestamps('created_at');
+
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null')->onUpdate('no action');
         });
     }
 
