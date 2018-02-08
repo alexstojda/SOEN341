07f702709414ec51670e98e64335efae38a1a3a0
niconoloco <nicolas.b.champagne@gmail.com>
2018-02-08 16:13:15 -0500

    added Comments table +controller+migrations

--- database/migrations/2018_02_08_210354_create_comments_table.php
+++ database/migrations/2018_02_08_210354_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAnswersTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateAnswersTable extends Migration
      */
     public function up()
     {
-        Schema::create('answers', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('q_id');
+            $table->integer('a_id');
             $table->integer('userid');
-            $table->text('answer_body');
-            $table->boolean('isBestAnswer');
+            $table->text('comment_body');
             $table->timestamps('created_at');
         });
     }
@@ -30,6 +30,6 @@ class CreateAnswersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('answers');
+        Schema::dropIfExists('comments');
     }
 }
