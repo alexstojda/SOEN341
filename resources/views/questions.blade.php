9f60d2aed02317559922922d8c8d5220d623ffdf
nbeeri <nbeeri@hotmail.com>
2018-02-14 18:16:13 -0500

    [#50]Added the up/down vote buttons to the answers page, plus a counter to show the number of votes

--- resources/views/questions.blade.php
+++ resources/views/questions.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+
+    @if(Auth::check())
     <div class="container">
         <a href="questions/create" role="button" class="btn btn-info btn-block">
             Add a Question
         </a>
     </div>
+    @endif
 
     <div class="container">
         @foreach ($questions as $question)
             <div class="text-center">
                 <h3><a href="questions/{{ $question->id }}">{{ $question->title }}</a></h3>
-
             </div>
         @endforeach
     </div>
