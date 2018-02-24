f32eaede6875a4b7e257bc9195e5191e1dd1a2c7
Incommensurable <h.j.halcro@gmail.com>
2018-02-23 21:59:09 -0500

    [#63] added bootstrap's jumbotron element and a few containers to the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,45 @@
         </status-toast>
     @endif
 
-    <dashboard-notification>
-        You are logged in!
-    </dashboard-notification>
+    <div class="jumbotron">
+        <div class="container">
+            <h1 class="display-3">Welcome!</h1>
+            <p>This is where we can put some text that is welcoming to the user! Get them to click the button below</p>
+            <!--TODO get that button smaller and a nicer colour-->
+            <p><a href="questions/" role="button" class="btn btn-info btn-block">
+                    Go To Questions!
+                </a></p>
+        </div>
+    </div>
 
     <div class="container">
-        <a href="questions/" role="button" class="btn btn-info btn-block">
-                Go To Questions!
-        </a>
+        <!-- Example row of columns -->
+        <div class="row">
+            <div class="col-md-4">
+                <h2>Could we put</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div>
+            <div class="col-md-4">
+                <h2>the most recent</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div>
+            <div class="col-md-4">
+                <h2>questions here?</h2>
+                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div>
+        </div>
+
+        <hr>
+
     </div>
+    <!--<dashboard-notification>
+        You are logged in!
+    </dashboard-notification>-->
+
+
 
 
 
