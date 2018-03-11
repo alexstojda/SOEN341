247f7c8ed77d4b89cd500dbc75b7ef91a0b60a9a
Ivan Ilyushchenko <outofphasor@users.noreply.github.com>
2018-03-10 20:09:45 -0500

    [#94] added vote actions (up/down) to vote controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
     namespace App\Http\Controllers;
 
+    use Illuminate\Database\Eloquent\ModelNotFoundException;
     use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
     use Illuminate\Foundation\Bus\DispatchesJobs;
     use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -9,4 +10,29 @@
 
     class Controller extends BaseController {
         use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+        protected function determineModel(string $model, int $id) {
+            try {
+                switch ($model) {
+                    case 'answers' :
+                        try {
+                            return \App\Answer::whereId($id)->first();
+                        } catch (ModelNotFoundException $exception) {
+                            return redirect()->back();
+                        }
+                        break;
+                    case 'questions' :
+                        try {
+                            return \App\Question::whereId($id)->first();
+                        } catch (ModelNotFoundException $exception) {
+                            return redirect()->back();
+                        }
+                        break;
+                    default :
+                        return redirect()->back();
+                }
+            } catch (ModelNotFoundException $exception) {
+                return redirect()->back();
+            }
+        }
     }
