8c4e1ca71ed2a7da16897293412fdf50c34abccb
Incommensurable <h.j.halcro@gmail.com>
2018-03-12 12:16:04 -0400

    [#67] comments vue components and ajax submit

---
+++ resources/assets/js/components/comment/CommentComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <li :id="'comment-'+comment.id" class="comment card list-group-item">
+        <!--div class="comment-actions">
+            <div class="comment-score">
+                <span title="number of 'useful comment' votes received" class="warm">12</span>
+            </div>
+        </div-->
+        <div class="comment-text card-body js-comment-text-and-form">
+            <div style="display: block;" class="comment-body">
+                <span class="comment-copy">
+                    {{ comment.body }}
+                </span>
+                –&nbsp;<a :href="'/users/'+comment.author.id" class="comment-user">{{comment.author.name}}</a>
+                <span class="comment-date " dir="ltr">
+                    <a class="comment-link" :href="'#comment-'+comment.id">
+                        <small :title="comment.date.full" class="relativetime-clean">{{comment.date.readable}}</small>
+                    </a>
+                </span>
+            </div>
+        </div>
+    </li>
+</template>
+
+<script>
+  export default {
+    name: 'comment',
+    props: {
+      comment: {}
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
