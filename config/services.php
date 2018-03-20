a703615d913d54f4a4bc85cbe88ceac7a5e6bc2d
Andrew H <andrew.hanichkovsky@mail.concordia.ca>
2018-03-20 14:34:16 -0400

    [#101] heroku fb callback fix

--- config/services.php
+++ config/services.php
@@ -17,7 +17,7 @@ return [
     'facebook' => [
         'client_id' => env('S_CLIENTID'),
         'client_secret' => env('S_SECRET'),
-        'redirect' => url('/callback'),
+        'redirect' => env('APP_URL'),
     ],
 
 
